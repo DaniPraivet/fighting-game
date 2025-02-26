b40aa4dcf0b0c47d5138e1e8b968d20bd15ee5f5
tonimatasdev <me@tonimatas.dev>
2025-02-26 23:05:45 +0100

    Transfer size to game panel, and use a thread instead of a timer

--- src/main/java/dev/danipraivet/juegodelucha/window/PanelJuego.java
+++ src/main/java/dev/danipraivet/juegodelucha/window/PanelJuego.java
@@ -6,21 +6,41 @@ import dev.danipraivet.juegodelucha.map.Plataforma;
 
 import javax.swing.*;
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
 public class PanelJuego extends JPanel {
+    private static final int FPS = 60;
+    private static boolean RUNNING = true;
+    
     private final Jugador jugador;
     private final Enemigo enemigo;
     private final Plataforma plataforma;
 
-    public PanelJuego() {
+    public PanelJuego(JFrame frame) {
         setBackground(Color.BLACK);
+        setSize(frame.getSize());
+        setVisible(true);
         jugador = new Jugador(1400, 100);
         enemigo = new Enemigo( 600, 100);
         plataforma = new Plataforma(360, 400, 1200, 100);
 
-        new Timer(16, e -> {
-            actualizar();
-            repaint();
+        new Thread(() -> {
+            while (RUNNING) {
+                long time =  System.currentTimeMillis();
+
+                actualizar();
+                repaint();
+
+                long elapsed = System.currentTimeMillis() - time;
+                long frameTime = 1000 / FPS;
+                long sleepTime = frameTime - elapsed;
+
+                try {
+                    TimeUnit.MILLISECONDS.sleep(sleepTime);
+                } catch (InterruptedException e) {
+                    System.out.println("Error generating next frame.");
+                }
+            }
         }).start();
     }
 
