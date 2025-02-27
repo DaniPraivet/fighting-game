9929df78facf27ff7a68c2f7e9be353a8d9378be
DaniPraivet <danipraivet@gmail.com>
2025-02-27 19:08:30 +0100

    Fixing dynamic positions depending on the resolution Part 1

--- src/main/java/dev/danipraivet/juegodelucha/window/VentanaJuego.java
+++ src/main/java/dev/danipraivet/juegodelucha/window/VentanaJuego.java
@@ -6,10 +6,12 @@ import javax.swing.*;
 
 public class VentanaJuego extends JFrame {
     public static PanelJuego PANEL;
+    public static final int ANCHO_VENTANA = 1280;
+    public static final int ALTO_VENTANA = 720;
 
     public VentanaJuego() {
         setTitle("Ñawlhalla");
-        setSize(1920, 1080);
+        setSize(ANCHO_VENTANA, ALTO_VENTANA);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         setVisible(true);
